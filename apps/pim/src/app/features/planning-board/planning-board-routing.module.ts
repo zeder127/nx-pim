f775a21303ba7edd13cbfa085b43d09eae90681c
Xuesong Wen <xuesong.wen@gmail.com>
2020-11-26 14:29:44 +0100

    chore: install

--- apps/pim/src/app/features/planning-board/planning-board-routing.module.ts
+++ apps/pim/src/app/features/planning-board/planning-board-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SyncBoardComponent } from './sync-board/sync-board.component';
+import { TeamBoardComponent } from './team-board/team-board.component';
 
 const routes: Routes = [
   {
     path: 'sync',
     component: SyncBoardComponent,
+  },
+  {
+    path: 'team/:teamName',
+    component: TeamBoardComponent,
   },
   {
     path: '**',
