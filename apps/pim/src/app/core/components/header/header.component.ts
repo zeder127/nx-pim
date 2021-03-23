537095169df3837527d078955db6dec23c615e33
Xuesong Wen <xuesong.wen@gmail.com>
2021-03-23 12:53:38 +0100

    fix: name of undefined in header

--- apps/pim/src/app/core/components/header/header.component.ts
+++ apps/pim/src/app/core/components/header/header.component.ts
@@ -63,7 +63,7 @@ export class HeaderComponent implements OnInit {
 
   checkoutAccount() {
     this.loggedIn = !!this.authService.getAccount();
-    this.userName = this.authService.getAccount().name;
+    this.userName = this.authService.getAccount()?.name;
     // this.authService.acquireTokenSilent({scopes: ['499b84ac-1321-427f-aa17-267ca6975798/.default']}).then(response => {
     //   console.log("🚀 ~ file: app.component.ts ~ line 47 ~ AppComponent ~ checkoutAccount ~ token", response)
 
