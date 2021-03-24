270dc116cbc89d6c4e258b74469ac56b679397d5
Xuesong Wen <xuesong.wen@gmail.com>
2021-03-24 08:59:49 +0100

    feat: pi switcher in header

--- apps/pim/src/app/core/components/header/header.component.ts
+++ apps/pim/src/app/core/components/header/header.component.ts
@@ -1,22 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { BroadcastService, MsalService } from '@azure/msal-angular';
+import { AutoUnsubscriber } from '@pim/ui';
 import { CryptoUtils, Logger } from 'msal';
+import { MenuItem } from 'primeng/api';
+import { filter } from 'rxjs/operators';
+import { PiService } from '../../../shared/services/pi.service';
 
 @Component({
   selector: 'pim-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent extends AutoUnsubscriber implements OnInit {
   public loggedIn = false;
   public userName: string;
+  public currentPiName: string;
+  public currentFeature = 'planning';
+  public currentTeamName: string;
+  public piMenuItems: MenuItem[];
 
   constructor(
     private broadcastService: BroadcastService,
-    private authService: MsalService
-  ) {}
+    private authService: MsalService,
+    private piService: PiService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const params = this.getParams(this.route);
+        this.currentPiName = params['piName'];
+      }
+    });
+
+    this.piService
+      .getPisAsync()
+      .pipe(
+        this.autoUnsubscribe(),
+        filter((pis) => !!pis)
+      )
+      .subscribe((pis) => {
+        this.piMenuItems = pis.map((pi) => {
+          if (pi.name === this.currentPiName)
+            return {
+              label: pi.name,
+              disabled: true,
+            };
+          else
+            return {
+              label: pi.name,
+              routerLink: `${this.currentFeature}/${pi.name}/board`,
+            };
+        });
+        this.piMenuItems.push({
+          label: 'New PI',
+          icon: 'pi pi-plus',
+          routerLink: 'admin/pi/new',
+        });
+      });
     this.checkoutAccount();
 
     this.broadcastService.subscribe('msal:loginSuccess', () => {
@@ -43,6 +90,22 @@ export class HeaderComponent implements OnInit {
         }
       )
     );
+  }
+
+  /**
+   * Get route parameters from outside router-outlet
+   * @param route
+   * @returns
+   */
+  private getParams(route: ActivatedRoute) {
+    let params = route.snapshot.params;
+    params = { ...route.snapshot.queryParams, ...params };
+    if (route.children) {
+      for (const r of route.children) {
+        params = { ...this.getParams(r), ...params };
+      }
+    }
+    return params;
   }
 
   login() {
--- apps/pim/src/app/core/core.module.ts
+++ apps/pim/src/app/core/core.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
 import { ToolbarModule } from 'primeng/toolbar';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule, ButtonModule, ToolbarModule],
+  imports: [CommonModule, RouterModule, ButtonModule, ToolbarModule, MenuModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
